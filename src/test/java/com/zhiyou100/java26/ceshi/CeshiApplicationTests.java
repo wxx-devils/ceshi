eaa3bb29f653af746833fe46dcd33b22be646c14
王小笑 <wxxdevils@qq.com>
2020-10-22 19:43:48 +0800

    Initial commit

---
+++ src/test/java/com/zhiyou100/java26/ceshi/CeshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhiyou100.java26.ceshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeshiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
